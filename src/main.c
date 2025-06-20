8e7e4b240539e0fa01a5bc2e1905a13161c9af66
FeltMacaroon389 <feltmacaroon@proton.ne>
2025-06-20 13:53:07 +0200

    improved comments

--- src/main.c
+++ src/main.c
@@ -1,11 +1,16 @@
 #include <efi.h>
 #include <efilib.h>
 
-EFI_STATUS
-efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+// Program entrypoint
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+
+	// Initialize the standard EFI library
 	InitializeLib(ImageHandle, SystemTable);
+
+	// Print message
 	Print(L"Hello, World!\n");
-	
+
+	// Return from program
 	return EFI_SUCCESS;
 }
 
